8144ca0e611cde2906ebd011b07403dc2d9d80dd
KevinRondeau <k.rondeau1991@hotmail.com>
2020-10-02 13:13:47 -0400

    initial commit

--- DogFetchApp/ViewModels/MainViewModel.cs
+++ DogFetchApp/ViewModels/MainViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace DogFetchApp.ViewModels
 {
     class MainViewModel : BaseViewModel
     {
-
+        ObservableCollection<DogModel> Races;
     }
 }
